376a29d3455411c78467c791fadf1d55ea61b05b
liaojiayun <liaojiayun@bytedance.com>
2023-08-25 14:25:40 +0800

    feat: rq basics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import CountProvider from "./components/CountProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import ReactQueryDevtools from "./components/ReactQueryDevTools";
+import ComponentA from "./components/ComponentA";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      {/* <ReactQueryDevtools /> */}
+      <ReactQueryDevtools initialIsOpen={false} />
+      <CountProvider>
+        {/* <App /> */}
+        <ComponentA />
+      </CountProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
