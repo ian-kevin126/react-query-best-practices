35b9686e23cd0e03a51a6982f0233f7cb5f4eae7
liaojiayun <liaojiayun@bytedance.com>
2023-08-28 10:11:07 +0800

    feat: finish base project

--- src/components/ReactQueryDevTools.tsx
+++ src/components/ReactQueryDevTools.tsx
@@ -1,28 +1,28 @@
-// import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
-// const ReactQueryDevtoolsProduction = React.lazy(() =>
-//   import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
-//     (d) => ({
-//       default: d.ReactQueryDevtools,
-//     })
-//   )
-// );
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/lib/index.prod.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
 
-// const ReactQueryDevtools = () => {
-//   const [showDevtools, setShowDevtools] = useState(false);
+const ReactQueryDevtools = () => {
+  const [showDevtools, setShowDevtools] = useState(false);
 
-//   useEffect(() => {
-//     window.toggleDevtools = () =>
-//       setShowDevtools((previousState) => !previousState);
-//   }, []);
+  useEffect(() => {
+    window.toggleDevtools = () =>
+      setShowDevtools((previousState) => !previousState);
+  }, []);
 
-//   return (
-//     showDevtools && (
-//       <Suspense fallback={null}>
-//         <ReactQueryDevtoolsProduction />
-//       </Suspense>
-//     )
-//   );
-// };
+  return (
+    showDevtools && (
+      <Suspense fallback={null}>
+        <ReactQueryDevtoolsProduction />
+      </Suspense>
+    )
+  );
+};
 
-// export default ReactQueryDevtools;
+export default ReactQueryDevtools;
--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,17 @@ import "./index.css";
 // import App from "./App";
 import CountProvider from "./components/CountProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import ReactQueryDevtools from "./components/ReactQueryDevTools";
+import ReactQueryDevtools from "./components/ReactQueryDevTools";
 import ComponentA from "./components/ComponentA";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      {/* <ReactQueryDevtools /> */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
       <CountProvider>
         {/* <App /> */}
         <ComponentA />
