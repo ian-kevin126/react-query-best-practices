5da3634b3f0ccfe6fd67c617c7f3b78c776b7602
liaojiayun <liaojiayun@bytedance.com>
2023-09-20 11:52:06 +0800

    feat: modify layout

---
+++ src/routes/Layout.tsx
@@ -0,0 +1,44 @@
+import { Button } from "antd";
+import { NavLink, Outlet } from "react-router-dom";
+
+interface INavListItem {
+  title: string;
+  to: string;
+}
+
+const commonStyle = {
+  border: "1px solid #d9d9d9",
+  padding: 10,
+  borderRadius: 8,
+  minHeight: 400,
+};
+
+const Layout = () => {
+  const navList: INavListItem[] = [
+    {
+      title: "useQuery基础用法",
+      to: "/users",
+    },
+    {
+      title: "QueryObserver 用法",
+      to: "/QueryObserver",
+    },
+  ];
+
+  return (
+    <div style={{ display: "flex", gap: 20 }}>
+      <div style={{ ...commonStyle, width: 200 }}>
+        {navList.map((item: INavListItem) => (
+          <NavLink style={{ display: "block" }} to={item.to}>
+            <Button type="link">{item.title}</Button>
+          </NavLink>
+        ))}
+      </div>
+      <div style={{ ...commonStyle, flex: 1 }}>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
-import Root from "./root";
 import UserList from "../pages/UserList";
 import QueryObserverDemo from "../pages/QueryObserver";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/users",
-    element: <UserList />,
-  },
-  {
-    path: "/QueryObserver",
-    element: <QueryObserverDemo />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <>create</>,
+      },
+      {
+        path: "/users",
+        element: <UserList />,
+      },
+      {
+        path: "/QueryObserver",
+        element: <QueryObserverDemo />,
+      },
+    ],
   },
 ]);
 
